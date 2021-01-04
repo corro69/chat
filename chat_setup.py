b7716e80e5b95a6a5df423e3cdd7038a2e1d5885
Dustin Brooks <dbrooks@linuxmail.org>
2021-01-04 16:48:59 -0600

    Update chat_setup.py

--- chat_setup.py
+++ chat_setup.py
@@ -6,7 +6,8 @@ from threading import Thread
 layout=[
     [sg.Text("Sever IP"), sg.InputText(), sg.OK()],
     [sg.Text("name"), sg.InputText(), sg.OK()],
-    [sg.Output(size=(50,18),key = 'OUTPUT')]
+    [sg.Output(size=(50,5),key = 'OUTPUT')],
+    [sg.Button("Done")]
 ]
 
 window = sg.Window("Chat Setup", layout, size=(300,150))
@@ -24,6 +25,9 @@ while True:
         pickle_out.close()
 
         print(server_info)
+        
+    if event == "Done":
+        window.close()
 
     if event == sg.WIN_CLOSED or event =="Cancel":
         break
