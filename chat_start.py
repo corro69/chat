9a1f2d4d491572e79d8747daf2d5704b51ec4c52
Dustin Brooks <dbrooks@linuxmail.org>
2021-01-05 20:36:35 -0600

    Update chat_start.py

--- chat_start.py
+++ chat_start.py
@@ -9,11 +9,11 @@ server_info = pickle.load(pickle_in)
 layout=[
     [sg.Text("Server:    "+ server_info[0])],
     [sg.Text("Chat Name:  "+ server_info[1])],
-    [sg.Button(("Change Login Info"),size=(50,0))],
+    [sg.Button(("Change Server IP and Chat ID"),size=(50,0))],
     [sg.Button(("Join Chat"),size=(50,0))]
 ]
 
-window = sg.Window("Chat Start", layout, size=(200,150))
+window = sg.Window("Chat Start", layout, size=(250,150))
 
 while True:
     event, values = window.read()
